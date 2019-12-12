cd6d377a105eb9981d7492be661c7bf6b1173625
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2019-12-12 15:00:59 +0100

    debug

--- vars/Compare.groovy
+++ vars/Compare.groovy
@@ -1,17 +1,20 @@
 enum Compare {
-    LessThan {
-        @Override
-        public boolean eval(val1, val2) {
-            return val1 < val2
-        }
-    },
-    GreaterThan {
-        @Override
-        public boolean eval(val1, val2) {
-            return val1 > val2
-        }
-    };
-
-
-    public abstract boolean eval(val1, val2);
+    
+    LessThan, GreaterThan
+    
+//    LessThan {
+//        @Override
+//        public boolean eval(val1, val2) {
+//            return val1 < val2
+//        }
+//    },
+//    GreaterThan {
+//        @Override
+//        public boolean eval(val1, val2) {
+//            return val1 > val2
+//        }
+//    };
+//
+//
+//    public abstract boolean eval(val1, val2);
 }
