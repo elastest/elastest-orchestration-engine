ecaaad77fd817519aef8fac2c3070fb0cb08b574
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2019-12-12 15:21:56 +0100

    fix

--- vars/TimeUnit.groovy
+++ vars/TimeUnit.groovy
@@ -1,30 +1,30 @@
 enum TimeUnit {
 
-    MILLISECONDS {
-        @Override
-        public long convertToMillis(time) {
-            return time
-        }
-    },
-    SECONDS{
-        @Override
-        public long convertToMillis(time) {
-            return time * 1000
-        }
-    },
-    MINUTES{
-        @Override
-        public long convertToMillis(time) {
-            return time * 60 * 1000
-        }
-    },
-    HOURS{
-        @Override
-        public long convertToMillis(time) {
-            return time * 60 * 60 * 1000
-        }
+    MILLISECONDS("MILLISECONDS"),
+    SECONDS("SECONDS"),
+    MINUTES("MINUTES"),
+    HOURS("HOURS")
+
+    String value;
+
+    private TimeUnit(String value) {
+        this.value = value
     }
 
-
-    public abstract long convertToMillis(time);
+    public long convertToMillis(time) {
+        switch(this.value) {
+            case "SECONDS":
+                return time * 1000
+                break;
+            case "MINUTES":
+                return time * 60 * 1000
+                break;
+            case "HOURS":
+                break;
+            case "MILLISECONDS":
+                return time * 60 * 60 * 1000
+            default:
+                return time;
+        }
+    }
 }
